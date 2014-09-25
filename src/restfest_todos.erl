2ba2b0f455b5987e99ee27af65cdedb0f9b89da8
Sean Cribbs <sean@basho.com>
2014-09-25 10:40:04 -0400

    Add simple todos store

---
+++ src/restfest_todos.erl
@@ -0,0 +1,33 @@
+-module(restfest_todos).
+-include("todos.hrl").
+-define(TAB, ?MODULE).
+
+-export([
+         open/0,
+         insert/1,
+         update/1,
+         find/1,
+         new_id/0
+        ]).
+
+open() ->
+    dets:open(?TAB, [{keypos, #todo.id}]).
+
+insert(T=#todo{}) ->
+    dets:insert_new(?TAB, T).
+
+update(T=#todo{id=ID}) ->
+    case dets:lookup(?TAB, ID) of
+        [_OtherTodo] -> dets:insert(?TAB, T);
+        _ -> false
+    end.
+
+find(ID) ->
+    case dets:lookup(?TAB, ID) of
+        [Todo=#todo{}]-> {ok, Todo};
+        _ -> false
+    end.
+
+
+new_id() ->
+    mochihex:to_hex(crypto:hash(sha1, term_to_binary({erlang:now(), erlang:make_ref()}))).
---
+++ src/todos.hrl
@@ -0,0 +1,9 @@
+-record(todo, {
+          id :: string(),
+          title :: string(),
+          dateDue :: calendar:date(),
+          notes :: string(),
+          dateCreated :: calendar:date(),
+          dateUpdated :: calendar:date()
+         }).
+          
