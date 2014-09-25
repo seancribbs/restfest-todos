47d952dcb97c0dff2ef2f7539a68fa246bf09dbb
Sean Cribbs <sean@basho.com>
2014-09-25 09:12:37 -0400

    Initial commit

---
+++ src/restfest_app.erl
@@ -0,0 +1,13 @@
+-module(restfest_app).
+
+-behaviour(application).
+-export([
+    start/2,
+    stop/1
+]).
+
+start(_Type, _StartArgs) ->
+    restfest_sup:start_link().
+
+stop(_State) ->
+    ok.
